11a469eccf93dad4500878eb82aa9846086809ed
Monika Bartulovic <bartu043@umn.edu>
2023-04-15 20:55:22 -0500

    adding in basic pages, nav bar

--- nextjs-starter/src/pages/_app.js
+++ nextjs-starter/src/pages/_app.js
@@ -1,5 +1,42 @@
-import '@/styles/globals.css'
+import Link from 'next/link' 
+import '@/styles/index.css'
+import { ClerkProvider } from '@clerk/nextjs'
+
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <header>
+    <ClerkProvider {...pageProps} >
+    </ClerkProvider>
+      <nav className='navbar'>
+        <Link href="/">
+          <img src="/BookTokLogo.png" alt="BookTok" className='navbar__logo' />
+        </Link>
+
+        <ul className='navbar__menu'>
+          <li>
+            <Link href="/search"> Search</Link>
+          </li>
+
+          <li>
+            <Link href="/map"> Map</Link>
+          </li>
+
+          <li>
+            <Link href="/profile"> Profile</Link>
+          </li>
+
+          <li>
+            <Link href="/login"> Login</Link>
+          </li>
+        </ul>
+
+        
+      </nav>
+    </header>
+
+    <Component {...pageProps} />  
+  </>
 }
---
+++ nextjs-starter/src/pages/login.js
@@ -0,0 +1,28 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
+
+
+export default function Login({ Component, pageProps }) {
+    const { isLoaded, userId, sessionId, getToken } = useAuth();
+return <>
+    {/* <ClerkProvider {...pageProps} >
+     
+    </ClerkProvider> */}
+    
+Login
+
+</>
+
+  }
+  
+
+
+
+
+// const SignUpPage = () => (
+//   <SignUp path="/login" routing="path" signInUrl="/login" 
+//    redirectUrl="/_app.js"/>
+//   );
+
+// export default SignUpPage;
