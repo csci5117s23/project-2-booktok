9c9a6022a37eb56e14a03612d59e36f153903711
fiorelaem <fiorela.e.martinez@gmail.com>
2023-04-17 10:55:33 -0500

    map code

--- nextjs-starter/src/pages/map.js
+++ nextjs-starter/src/pages/map.js
@@ -1,9 +1,33 @@
-import { UserButton } from "@clerk/clerk-react";
+import React from "react";
+import GoogleMapReact from 'google-map-react';
 
+const mapKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY;
 
-export default function ToDos() {
-  return <>
-    <h1>Map Page</h1>
-    <UserButton afterSignOutUrl="/"/>
-  </>
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: 44.9740,
+      lng: -93.2277
+    },
+    zoom: 11
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: mapKey }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={44.9740}
+          lng={-93.2277}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
 }
