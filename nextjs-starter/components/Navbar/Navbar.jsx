18cc1d98a20f97b3fe00bd4e6722e4c854dd2ef3
sollse <kim01540@umn.edu>
2023-04-24 01:46:07 -0500

    fix nav bar

--- nextjs-starter/components/Navbar/Navbar.jsx
+++ nextjs-starter/components/Navbar/Navbar.jsx
@@ -1,45 +1,47 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn, UserButton } from '@clerk/nextjs'
 
 export default function Navbar() {
+  const [clicked, setClick] = useState(false);
+
   return (
     <header>
-      <nav className="navbar is-warning" role="navigation" aria-label="main navigation">
+      <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <Link href="/">
+          <Link className="navbar-item" href="/">
             YumYum Tok
+            {/* NOTE: Have to make a new logo or just use word */}
             {/* <img src="/BookTokLogo.png" alt="BookTok" className='navbar__logo' /> */}
           </Link>
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <a role="button" onClick={() => {setClick(!clicked);}} className={`navbar-burger burger ${clicked ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
+        
+        {/* Hidden in mobile device */}
+        <div id="navbarBasicExample" className={`navbar-menu ${clicked ? "is-active" : ""}`}>
           <div className="navbar-start">
+            {/* <div className='navbar-item'>aaaa</div> */}
             <Link className='navbar-item' href="/search"> Search</Link>
             <Link className='navbar-item' href="/map"> Map</Link>
             <Link className='navbar-item' href="/profile"> Profile</Link>
             <Link className='navbar-item' href="/camera"> Camera</Link>
             <UserButton className='navbar-item' afterSignOutUrl="/" />
           </div>
+
+          <div className="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-light">
+                  Log out
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-
-        
-          {/* <li>
-            <Link href="/profile"> Profile</Link>
-          </li>
-
-          <li>
-            <Link href="/camera"> Camera</Link>
-          </li>
-
-          <li>
-            <UserButton afterSignOutUrl="/" />
-          </li>
-        </ul> */}
       </nav>
     </header>
   );
