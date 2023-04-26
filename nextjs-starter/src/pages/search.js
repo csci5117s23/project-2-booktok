0ba579623a3f37751ad6f7f8ffffee4b4936ffd1
hoin0784 <hoin0784@gmail.com>
2023-04-26 00:41:38 -0500

    Deleted unused import[C

--- nextjs-starter/src/pages/search.js
+++ nextjs-starter/src/pages/search.js
@@ -1,9 +1,7 @@
 import SearchHeader from '../../components/SearchHeader/SearchHeader';
 import Categories from '../../components/Categories/Categories';
-import { useGeoLocation } from 'use-geo-location';
 
 export default function Search() { 
-  const { latitude, longitude} = useGeoLocation(); 
   const categories = ['Burgers', 'Japanese', 'Korean', 'Chinese', 'Italian', 'Mexican', 'Thai', 'Pizza'];
   
   return (
