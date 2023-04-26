823cc805f0d87ce00e47d7bf67d9f780e97e767d
sollse <kim01540@umn.edu>
2023-04-26 12:21:12 -0500

    change home pag

--- nextjs-starter/src/pages/home.js
+++ nextjs-starter/src/pages/home.js
@@ -7,7 +7,7 @@ import { useAuth } from "@clerk/nextjs";
 import React, { useState, useEffect, useCallback } from "react";
 import {Camera} from './camera.js'
 
-export default function HomePage() {
+export default function ReviewPage() {
 
     const [loading, setLoading] = useState(true);
     const [restaurants, setRestaurants] = useState([]);
@@ -76,18 +76,117 @@ export default function HomePage() {
 
         return (
         <>  
-            {/* {console.log("t1: ", restaurants)}
-            {restaurantListItems} */}
+            {/* Review Form */}
+            {/* <h2>Add to your Timeline:</h2> */}
+
             <div className="columns is-centered">
+                <form className="box mx-5">
+                    <div className="field">
+                        <label className="label">Restaurant</label>
+                        <div className="control">
+                            <input
+                                className="input"
+                                type="text"
+                                id = "restaurant"
+                                placeholder="Restaurant Name"
+                                value={newName}
+                                onChange={(e) => setNewName(e.target.value)}
+                                onKeyDown = {(e)=>{if (e.key === 'Enter'){add()}}}
+                            ></input>
+                        </div>
+                        <p class="help is-success">This field is required</p>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Review</label>
+                        <div className="control">
+                            <input
+                                className="input"
+                                id = "review"
+                                type="textarea"
+                                value={newReview}
+                                onChange={(e) => setNewReview(e.target.value)}
+                                onKeyDown = {(e)=>{if (e.key === 'Enter'){add()}}}
+                            ></input>
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Rating</label>
+                        <div className="control">
+                            {/* <Typography component="legend">Rating:</Typography> */}
+                            <Rating name="half-rating" 
+                                id = "rating"
+                                defaultValue={0} 
+                                precision={0.5} 
+                                value = {newRating}
+                                onChange={(e) => setNewRating(e.target.value)}
+                                onKeyDown={(e) => {if(e.key === 'Enter'){add()}}}/>
+                        </div>
+                        <p class="help is-success">This field is required</p>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Image</label>
+                        <div className="control">
+                        {/* <Typography component="legend">Upload an image:</Typography> */}
+                        {newImage && (
+                            <div>
+                                <img
+                                    alt="not found"
+                                    width={"250px"}
+                                    src={URL.createObjectURL(newImage)}
+                                />
+                            <br />
+                            <button onClick={() => setNewImage(null)}>Remove</button>
+                            </div>    
+                        )}
+                        <input
+                            type="file"
+                            name="myImage"
+                            onChange={(e) => {
+                                console.log(e.target.files[0]);
+                                setNewImage(e.target.files[0])} 
+                            }
+                            // onChange={(e) => setNewImage(e.target.value)}
+                            // onKeyDown={(e) => {if(e.key === 'Enter'){add()}}}/>
+                        />
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Date of the visit</label>
+                        <div className="control">
+                            <input
+                                id = "date"
+                                type="date"
+                                value={newDate}
+                                onChange={(e) => setNewDate(e.target.value)}
+                                onKeyDown = {(e)=>{if (e.key === 'Enter'){add()}}}
+                            ></input>
+                        </div>
+                    </div>
+
+                    <div className="field is-grouped">
+                        <div className="control">
+                            <button className="button is-success" onClick={add}>Add</button>
+                        </div>
+                    </div>
+                </form>
+            
+
+
+            {/* Loading review timeline */}
+                <br></br>
+            {/* <div className="columns is-centered"> */}
                 <div className="column is-half">
                 {/* <div className="column is-two-thirds"> */}
                     <h1 className={styles.titleTimeline}>Timeline</h1>
-                    {/* <div className="box"> */}
                         {console.log("t1: ", restaurants)}
                         {restaurantListItems}
-                    {/* </div>                   */}
                 </div>
             </div>
+
        
         </>
         );
